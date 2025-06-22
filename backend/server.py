8cb5c028b07ec3f5ba203d4251dc256f811bf9e6
Endergr <crzdennis6@gmail.com>
2025-06-22 15:55:17 +0000

    Rename and update backend API for Runway integration

--- backend/server.py
+++ backend/server.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import shutil
 import asyncio
 import random
-from api_runway import router as runway_router
+from api.runway import router as runway_router
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
